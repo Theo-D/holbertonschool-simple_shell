542517247017a71e8741ca814fd84491ec91ca86
Theo-D <theodessaigne@hotmail.fr>
2025-04-15 11:11:32 +0200

    Modified loop to effectively go through it

--- getCmd.c
+++ getCmd.c
@@ -6,20 +6,22 @@ void getCmd(char **av)
 {
 	int i = 0;
 	cmd_t cmdFunc[] = {
-		{"exit", exitCmd},
 		{"env", envCmd},
+		{"exit", exitCmd},
 		{NULL}
 	};
 
 	while (cmdFunc[i].cmd != NULL)
 	{
-		if (cmdFunc[i].cmd == av[0])
-			cmdFunc[i].fun(av);
-		else
+		if (strcmp(cmdFunc[i].cmd, av[0]) == 0)
 		{
-			printf("Error: Command not found");
+			cmdFunc[i].fun(av);
 			return;
 		}
+
+		/*Implement command not found handling*/
+		/*else
+			printf("ERRROR: Command not found");*/
 		i++;
 	}
 }
