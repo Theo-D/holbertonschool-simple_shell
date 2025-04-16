935798855bad3ecef60c3076c29bf1009730119d
Theo-D <theodessaigne@hotmail.fr>
2025-04-16 11:51:46 +0200

    Added call to executeCmd.c if no builtin command are found

--- getCmd.c
+++ getCmd.c
@@ -4,24 +4,24 @@
  */
 void getCmd(char **av)
 {
-	int i = 0;
+	int i = 0, errCmd = 0;
 	cmd_t cmdFunc[] = {
 		{"env", envCmd},
 		{"exit", exitCmd},
 		{NULL}
 	};
-
 	while (cmdFunc[i].cmd != NULL)
 	{
-		if (strcmp(cmdFunc[i].cmd, av[0]) == 0)
+		if ((errCmd = strcmp(cmdFunc[i].cmd, av[0])) == 0)
 		{
 			cmdFunc[i].fun(av);
-			return;
+			break;
 		}
-
-		/*Implement command not found handling*/
-		/*else
-			printf("ERRROR: Command not found");*/
 		i++;
 	}
+	/*printf("errCmd en sortie de boucle: %d\n", errCmd);*/
+	executeCmd(av);
+	/*printf("errCmd après executeCmd: %d\n", errCmd);*/
+	if (errCmd < 0)
+		printf("%s: Command not found\n", av[0]);
 }
