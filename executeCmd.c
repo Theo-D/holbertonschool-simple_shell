4791f1bf1745a80480586298d3077e430db4cd31
Theo-D <theodessaigne@hotmail.fr>
2025-04-17 13:24:20 +0200

    changed condition syntax to make and use of variables to make code clearer

--- executeCmd.c
+++ executeCmd.c
@@ -9,9 +9,9 @@
 int executeCmd(char **av)
 {
 	pid_t errFork = 0;
-	int errExec = 0, status = 0;
+	int  status = 0;
 
-	if (av == NULL)
+	if (av == NULL || av[0] == NULL)
 	{
 		exit(EXIT_FAILURE);
 	}
@@ -28,18 +28,20 @@ int executeCmd(char **av)
 		 * fork should be successful here, we are inthe child process
 		 * so we execute the file named after the command.
 		 */
-		errExec = execve(av[0], av, environ);
-		if (errExec)
+		if (execve(av[0], av, environ) == -1)
 		{
 			perror("Could not execute command");
 			exit(-1);
 		}
 	}
+	else
+	{
 	/**
 	 * If fork is successfull the following code will execute in the parent
 	 * process, so we wait the child to terminate.
 	 */
-	wait(&status);
-	/*equivalent to waitpid(-1, &status, 0)*/
+		wait(&status);
+		/*equivalent to waitpid(errFork, &status, 0)*/
+	}
 	return (0);
 }
