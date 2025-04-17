979804f687b8f778826d46b07f10acc5b9541aa5
Theo-D <theodessaigne@hotmail.fr>
2025-04-17 11:25:57 +0200

    Added envCmd.c and exitCmd.c that respectively prints environment variables and calls exit() system call.

--- envCmd.c
+++ envCmd.c
@@ -1,13 +1,16 @@
 #include "shell.h"
 /**
+ * envCmd - Prints array of environment variables.
+ * @av: Array of strings.
  *
+ * Return: 0 on succes, -1 on failure
  */
-int envCmd( __attribute__((unused))char **av)
+int envCmd(__attribute__((unused))char **av)
 {
 	int i = 0;
 
 	if (environ == NULL)
-		return (1);
+		return (-1);
 
 	while (environ[i])
 	{
