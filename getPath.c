a9b195a5970087193954c66aa242e7ca6d3778e5
Theo-D <theodessaigne@hotmail.fr>
2025-04-23 14:49:25 +0200

    Added getPath.c and getExecPath.c that first get and parses PATH line from environ into list of strings, then compares them to actual path + cmd name to find executable input by user. Updated shell.h to include declaration for both functions.

---
+++ getPath.c
@@ -0,0 +1,42 @@
+#include "shell.h"
+/**
+ *
+ */
+char **getPath(void)
+{
+	char **envCpy;
+	char *path = "PATH";
+	char *subStr = strdup(path);
+	char *pathLine;
+	char **pathArr = NULL;
+	int i = 0, lineLen = 0;
+
+	if (environ == NULL || subStr == NULL)
+		exit(2);
+
+	envCpy = environ;
+
+	while (*envCpy != NULL)
+	{
+		strncpy(subStr, envCpy[i], 4);
+		subStr[4] = '\0';
+
+		if (strcmp(subStr, path) == 0)
+			break;
+		i++;
+	}
+
+	free(subStr);
+	
+	lineLen = strlen(envCpy[i]);
+	pathLine = malloc((lineLen - 5) + 1);
+	strncpy(pathLine, envCpy[i] + 5, lineLen - 5);
+	pathLine[lineLen - 5] = '\0';
+
+	if (pathLine == NULL)
+		exit (2);
+
+	pathArr = tokenizeInput(pathLine);
+
+	return (pathArr);
+}
